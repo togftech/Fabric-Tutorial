63fc2be1dcb02925eaf4bbcdf8d357002c1a1db4
togftech <togftech@protonmail.com>
2022-10-15 09:59:24 +0100

    first day

--- src/main/java/net/togftech/tutorialmod/TutorialMod.java
+++ src/main/java/net/togftech/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.togftech.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.togftech.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/togftech/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.togftech.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.togftech.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final Item RAW_LITHIUM = registerItem("raw_lithium",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item LITHIUM = registerItem("lithium",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
